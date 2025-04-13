744ecd5242a4f7b713121a5d9ac23e116b8b8f67
Butan123-Git <provatchat945@gmail.com>
2025-04-13 03:28:52 +0000

    1st Script

---
+++ classes/Shellscripting/script1.sh
@@ -0,0 +1,5 @@
+echo "This is out first script in PW Skills"
+
+echo "We are going to learn some best practices of Shellscripting as well as automation through Bashscript"
+
+echo "Let's start our journey"
