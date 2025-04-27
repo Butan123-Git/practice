96b34f97800dc5510752f63ccd7c8d1c24c470fc
Butan123-Git <provatchat945@gmail.com>
2025-04-27 11:32:22 +0000

    Changes done for testing regex

--- classes/Shellscripting/script1.sh
+++ classes/Shellscripting/script1.sh
@@ -3,3 +3,10 @@ echo "This is out first script in PW Skills"
 echo "We are going to learn some best practices of Shellscripting as well as automation through Bashscript"
 
 echo "Let's start our journey"
+
+string="123456789"
+if [[ $string =~ [0-9]{12} ]]; then
+    echo "Number is valid" 
+    else 
+    echo "not matched"
+    fi
